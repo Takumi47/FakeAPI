2ba0a91e754f59027b8cfde9a6b234af9aa1c717
xander <xander@mail.com>
2023-05-23 00:05:27 +0800

    Initial Commit

---
+++ FakeAPI/FakeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeAPIApp.swift
+//  FakeAPI
+//
+//  Created by xander on 2023/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FakeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
